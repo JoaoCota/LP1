a25ad2306182ae32816ae1084d2da858e7e16812
JoaoCota <31344255+JoaoCota@users.noreply.github.com>
2018-09-12 09:59:47 -0300

    Add files via upload

---
+++ ex-2.1/07.c
@@ -0,0 +1,32 @@
+
+#include <stdio.h>
+#include <string.h>
+
+int main (void) {
+	char nome[20], N[20], n[20];
+	int i=1, v, e, d, ponto_atual, P=0, p=1;
+	while(i<=2){
+		printf("Digite o nome do time %d: \n", i);
+		scanf("%s", nome);
+		printf("Digite o numero de vitorias: \n");
+		scanf("%d", &v);
+		printf("Digite o numero de empates: \n");
+		scanf("%d", &e);
+		printf("Digite o numero de derrotas: \n");
+		scanf("%d", &d);
+		ponto_atual = (v*3) + e;
+		if(ponto_atual>P){
+			P = ponto_atual;
+			strcpy(N,nome);
+		}
+		else{
+			p = ponto_atual;
+			strcpy(n,nome);
+		}
+		
+		i+=1;
+	}
+	printf("Time %s tem mais pontos.\n", N);
+	printf("Time %s tem menos pontos.\n", n);
+    return 0;
+}
