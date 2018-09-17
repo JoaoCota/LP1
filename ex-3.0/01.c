02133e5d149ee844a6dd9392fa23a3356f583590
JoaoCota <31344255+JoaoCota@users.noreply.github.com>
2018-09-17 18:42:59 -0300

    Add files via upload

---
+++ ex-3.0/01.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+	char temps[50];
+	short temp, x, count = 0, media;
+	for (x=0; x<=2; x++)
+	{
+		printf("Digite uma temperatura entre -100  e 100. \n");
+		scanf("%hd", &temp);
+		temps[x] = temp;
+		if ((temp < -100) || (temp > 100))
+		{
+			printf("\n ERRO --> Digite apenas temperaturas entre -100  e 100. \n\n");
+			x--;
+		}
+		else
+		{
+			count += temp;
+		}
+	}
+	media = count/3;
+	printf("Media das temperaturas --> %hd.", media);
+}
