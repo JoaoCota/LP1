3531dd6839c9ea99665a130bf44075e12a8288c9
JoaoCota <31344255+JoaoCota@users.noreply.github.com>
2018-10-01 09:01:56 -0300

    Update 01.c

--- ex-4.2/01.c
+++ ex-4.2/01.c
@@ -1,30 +1,34 @@
 #include <stdio.h>
 
-int todos_os_primos(int n)
+int eh_primo(int n)
 {
 	int i, count=0;
+	for (i=1; i<=n; i++)
+	{
+		if (n%i == 0)
+		{
+			count++;
+		}
+	}
+	if (count == 2)
+	{
+		printf("--> %d \n", n);
+	}
+}
+
+int todos_os_primos(int n)
+{
 	while (n>0){
-		for (i=1; i<=n; i++)
-		{
-			if (n%i == 0)
-			{
-				count++;
-			}
-		}
-		if (count == 2)
-		{
-			printf("--> %d \n", n);
-		}
+		int x = eh_primo(n);
 		n--;
-		count=0;	
 	}
 }
 int main(void)
-{	
+{
 	int max;
 	printf("Digite um numero para exibir os primos menores que ele: \n\n");
 	scanf("%d", &max);
-	int x = todos_os_primos(max);
+	int y = todos_os_primos(max);
 	printf("\n================================ \n :FIM DO PROGRAMA:");
 	return 0;
 }
