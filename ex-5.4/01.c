982d9d98a170788eb3021b0b0325bdfe419990d7
JoaoCota <31344255+JoaoCota@users.noreply.github.com>
2018-10-10 09:38:26 -0300

    Add files via upload

--- ex-5.4/01.c
+++ ex-5.4/01.c
@@ -1,1 +1,20 @@
 
+#include <stdio.h>
+
+void troca(int* p1, int* p2)
+{
+    int aux;
+    aux = *p1;
+    *p1 = *p2;
+    *p2 = aux;
+}
+
+int main(void)
+{
+    int x = 10, y = 20;
+    p1 = &x;
+    p2 = &y;
+    troca(p1, p2);
+    printf("x = %d y = %d\n", x, y);
+    return 0;
+}
