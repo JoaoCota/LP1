75cee719caf31a113d57fa28a7b267790903838a
JoaoCota <31344255+JoaoCota@users.noreply.github.com>
2018-10-24 07:57:23 -0300

    Update 01.c

--- ex-5.2/01.c
+++ ex-5.2/01.c
@@ -1,42 +1,27 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-void compra(int* p, int valor){
-    int minha_conta = *p-valor;
-    printf("Saldo conta utilizada: R$ %d.00", minha_conta);
-}
-
-int main()
+void compra(int* conta, int valor)
 {
-    int minha_conta1;
-    int minha_conta2;
-    int* p;
-    int* p1;
-    int valor;
-
+    *conta = *conta - valor;
+}
+int main(void)
+{
+    int conta1, conta2, valor;
+    int* conta;
+    valor = 500;
     printf("Digite o saldo da conta 1: ");
-    scanf("%d", &minha_conta1);
+    scanf("%d", &conta1);
     printf("Digite o saldo da conta 2: ");
-    scanf("%d", &minha_conta2);
-    printf("Digite o valor da compra: ");
-    scanf("%d", &valor);
-    printf("\nSaldo antes da compra\n------------------------------------------\n");
-    printf("A conta 1 possui um saldo de R$ %d.00", minha_conta1);
-    printf("\nA conta 2 possui um saldo de R$ %d.00\n", minha_conta2);
-    printf("------------------------------------------\n");
-
-    if (minha_conta1>minha_conta2){
-        p=&minha_conta1;
-        p1=&minha_conta2;
+    scanf("%d", &conta2);
+    if (conta1 > conta2)
+    {
+        conta = &conta1;
     }
-    else{
-        p=&minha_conta2;
-        p1=&minha_conta1;
+    else
+    {
+        conta = &conta2;
     }
-    printf("\nSaldo final\n------------------------------------------\n");
-    compra(p, valor);
-    printf("\nSaldo conta nao utilizada: R$ %d.00\n", *p1);
-    printf("------------------------------------------\n");
-
+    compra(conta, valor);
+    printf("Saldo das contas apos compra:\nCONTA 1: %d\nCONTA 2: %d", conta1, conta2);
     return 0;
 }
