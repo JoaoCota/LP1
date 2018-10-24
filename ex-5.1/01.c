4e2562f22217b5efe7c24b3fe59b476b9168d194
JoaoCota <31344255+JoaoCota@users.noreply.github.com>
2018-10-24 07:58:01 -0300

    Update 01.c

--- ex-5.1/01.c
+++ ex-5.1/01.c
@@ -2,24 +2,23 @@
 
 
 int main(void)
-{	
-	int a, b;
-	int* p;
-	printf("Digite um numero --> ");
-	scanf("%d", &a);
-	printf("\nDigite um numero --> ");
-	scanf("%d", &b);
-	if(a > b)
-	{
-		int* p = &a;
-		*p = a-50;
-		printf("%d, %d.", *p, b);
-	}
-	else
-	{
-		int* p = &b;
-		*p = b-50;
-		printf("%d, %d.", *p, a);	
-	}
-	return 0;
+{
+    int a, b;
+    int* p;
+    printf("Digite o valor de A: ");
+    scanf("%d", &a);
+    printf("Digite o valor de B: ");
+    scanf("%d", &b);
+    printf("\nA = %d\nB = %d\n", a, b);
+    if (a > b)
+    {
+        p = &a;
+    }
+    else
+    {
+        p = &b;
+    }
+    *p = *p - 50;
+    printf("\nNovo A = %d\nNovo B = %d\n\n", a, b);
+    return 0;
 }
